1f31e54e4cb7206bd17a1605e034bf1d2cbe973e
Angel Minkov <aminkov@vmware.com>
2019-01-23 11:59:17 +0200

    new

---
+++ GuessTheNumber.py
@@ -0,0 +1,4 @@
+import random
+num = random.randint(1,100)
+usernum = int(input("Please, enter the number: "))
+print(num)
---
+++ work.py
@@ -0,0 +1,5 @@
+x = 0
+if not x % 2 == 0:
+        print(f"{x} is EVEN.")
+else:
+        print(f"{x} is EVEN.")
