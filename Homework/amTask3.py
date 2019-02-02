aef1d57f2aa6ea9fd09023b5608b0450b7656752
aminkov <aminkov@vmware.com>
2019-02-02 22:11:41 +0200

    tast3

---
+++ Homework/amTask3.py
@@ -0,0 +1,11 @@
+#Write a program which will prints out the sum of the all even numbers in the interval 1000, 1200 (both included).
+#111100
+
+
+a=0
+# why should I define a ????
+
+for i in range(1000,1201, 2):
+    a += i
+print(a)
+
