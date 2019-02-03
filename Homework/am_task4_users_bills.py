3c733bf33ba89415e8fa3be7ee57dab701f38e9e
aminkov <aminkov@vmware.com>
2019-02-03 12:39:44 +0200

    task4

---
+++ Homework/am_task4_users_bills.py
@@ -0,0 +1,24 @@
+# the data
+# A simplified data for telecom users are stored as two relational tables:
+#
+#users data table
+#  id |  name   |   number
+# ---|---------|---------------
+# 1  | "Maria" | "+39587111111"
+# 2  | "Ivan"  | "+39587222222"
+# 3  | "Asen"  | "+39587333333"
+#
+#users bills table
+# id |  bill
+# ---|---------
+# 1  | 25.50
+# 2  | 30.48
+# 3  |  5.98
+#
+# the task:
+# Represent the data in appropriate data structures in order to:
+# being able to add/remove records from each table
+# find and print the name of the user with highest bill
+# find and print the name of the user with lowest bill
+#  The user with highest bill - 30.48 is Ivan
+# The user with lowest bill - 5.98 is Asen
