d450b79d55d1be8b17f459cb3a22ad686da61f42
Angel Minkov <aminkov@vmware.com>
2019-02-06 12:37:08 +0200

    raise

--- Feb6-Exceptions-Objects/catching_exceptions.py
+++ Feb6-Exceptions-Objects/catching_exceptions.py
@@ -2,8 +2,10 @@
 #we can think of it as an if/else statement - try/except
 try:
     w = float(input('weight:'))
-except:
-    print('Please, enter a number')
+except EOFError:
+    print('THis is a EOFError...')
+except ValueError:
+    print('This is a ValueError, did you enter a number?')
 else:
     print('If there is no exception, this will be printed')
 finally:
@@ -12,3 +14,9 @@ finally:
 print(w)
 
 print('END of the program')
+
+#You can also use 'raise' to throw an exception
+
+str = input('Enter a name: ')
+if (str == 'pesho'):
+    raise KeyboardInterrupt
