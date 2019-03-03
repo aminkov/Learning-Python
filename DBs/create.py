89691a0f3303068797a1af736f56f517402070d7
aminkov <aminkov@vmware.com>
2019-03-03 17:37:48 +0200

    drop

--- DBs/create.py
+++ DBs/create.py
@@ -2,5 +2,5 @@ import sqlite3
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE jobstats
-             (date text, totaljobs int, newjobs int)''')
+             (date text, time text, totaljobs int, newjobs int)''')
 c = conn.cursor()
--- DBs/db.py
+++ DBs/db.py
@@ -6,11 +6,7 @@ import datetime
 page = requests.get('http://jobs.bg/index.php')
 tree = html.fromstring(page.content)
 jobs = tree.xpath('//span[@style="color:#8dbe35;font-weight:bold;"]/text()')
-# file = open("jobsnumber.txt","a")
-# file.write(str(datetime.datetime.now())) 
-# file.write("    ")
-# file.write(str(jobs) + '\n')
-# file.close()
+
 print(jobs[0])
 print(jobs[1])
 conn = sqlite3.connect('example.db')
