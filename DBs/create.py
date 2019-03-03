61c06b0cacec082648e9fb0d83562512affecccb
aminkov <aminkov@vmware.com>
2019-03-03 17:56:45 +0200

    3

--- DBs/create.py
+++ DBs/create.py
@@ -1,6 +1,11 @@
 import sqlite3
 conn = sqlite3.connect('example.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE jobstats
-             (date text, time text, totaljobs int, newjobs int)''')
-c = conn.cursor()
+
+
+def createtable():
+    c = conn.cursor()
+    c.execute('''CREATE TABLE jobstats
+                (date text, time text, totaljobs int, newjobs int)''')
+    c = conn.cursor()
+
+createtable()
