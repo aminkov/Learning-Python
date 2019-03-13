236b535fb4cdd28baa395bd59e93846dbab6244a
Angel Minkov <aminkov@vmware.com>
2019-03-13 13:12:59 +0200

    br

--- Mar13/regular_expression.py
+++ Mar13/regular_expression.py
@@ -1,10 +1,10 @@
 import re
 
 # the string to search with regex:
-user_email = "prefix@domain.er@te436"
+user_email = "pr34efix@domain.erte436-"
 
 # the regex to find if the userEmail contains '@' symbol:
-regex = re.compile(r'@')
+regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
 # do the match test:
 if regex.search(user_email):
