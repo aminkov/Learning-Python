12973659bce7c6922afae0e4eb5872ca3288d0a8
Angel Minkov <aminkov@vmware.com>
2019-03-18 11:59:29 +0200

    g

--- Mar_18_Data_Science/matplotlib_test.py
+++ Mar_18_Data_Science/matplotlib_test.py
@@ -5,12 +5,13 @@ import numpy as np
 x = np.arange(1,10)
 y = np.sqrt(x)
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
+#fig, ax = plt.subplots()
+#ax.plot(x, y)
+plt.plot(x, y)
 
-ax.set(xlabel='number (x)', ylabel='square root',
-       title='Square roots of numbers')
-ax.grid()
+#ax.set(xlabel='number (x)', ylabel='square root',
+#       title='Square roots of numbers')
+#ax.grid()
 
-fig.savefig("test.png")
+#fig.savefig("test.png")
 plt.show()
