4cc717249bc72499490e680f5161e8b31ca64616
Angel Minkov <aminkov@vmware.com>
2019-03-18 11:47:55 +0200

    br

---
+++ Mar_18_Data_Science/matplotlib_test.py
@@ -0,0 +1,16 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(1,10)
+y = np.sqrt(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+ax.set(xlabel='number (x)', ylabel='square root',
+       title='Square roots of numbers')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
--- Mar_18_Data_Science/numpy_test.py
+++ Mar_18_Data_Science/numpy_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import time
+import matplotlib.pyplot as plt
 
 bigrange = list(range(1,5000001))
 big_list = np.arange(1,5000001)
